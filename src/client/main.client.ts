d051a9b1194465c198b3db766b0c465ba871f072
zyilet <zhaoyims@outlook.com>
2023-10-27 16:34:11 +0800

    调整代码位置

--- src/client/main.client.ts
+++ src/client/main.client.ts
@@ -1,6 +1,6 @@
 import { KnitClient as Knit } from "@rbxts/knit";
 import { Component } from "@rbxts/knit"
-import { GameRunner } from "./GameRunner";
+import { GameRunner } from "./Game/GameRunner";
 
 Knit.AddControllers(script.Parent!.FindFirstChild("Controllers") as Folder);
 Component.Auto(script.Parent!.FindFirstChild("Components") as Folder);
@@ -9,7 +9,7 @@ Knit.Start()
     .andThen(() => {
 
         print("Clinet Started");
-        
+
         //启动游戏
         new GameRunner().Run()
     })
